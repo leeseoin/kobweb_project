1b786c83d3f65d05dae34aaf0855ed9ac5ac8911
kim <kimchy20013@gmail.com>
2025-10-22 21:26:58 +0900

    Remove old files and update project structure

--- kobweb_back/src/main/java/com/kob_backend_seoin/kob_backend/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kob_backend_seoin.kob_backend.repository;
-
-import com.kob_backend_seoin.kob_backend.domain.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProfileRepository extends JpaRepository<Profile, UUID> {
-} 
